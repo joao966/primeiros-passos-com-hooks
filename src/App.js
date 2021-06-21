b408d2c9f2ad93902cc960e0b9b9a27c0cbeafa4
joao966 <joaooliveira966@gmail.com>
2021-06-20 23:06:57 -0300

    Boas Vindas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import React, {useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [nome, setNome] = useState('');
+
+  const handleNome = () => {
+    return setNome('João');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello Word</p>
+        {nome && (<span>Seja bem-vindo: {nome}</span>)}
+        <button onClick={handleNome} >Boas-Vindas</button>
       </header>
     </div>
   );
